8c8f4c9b75debbf41efd26a0e2e3b9eb1e436759
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-12-28 21:16:16 -0400

    creating wallets controller tests

---
+++ tests/IntegrationTests.Desorganizze/Controllers/Wallets/WalletsControllerTests.cs
@@ -0,0 +1,11 @@
+﻿using IntegrationTests.Desorganizze.Utils;
+using Xunit;
+
+namespace IntegrationTests.Desorganizze.Controllers.Wallets
+{
+    [Collection("Server collection")]
+    public class WalletsControllerTests : IntegrationTest
+    {
+        public WalletsControllerTests(ServerFixture serverFixture) : base(serverFixture) {}
+    }
+}
