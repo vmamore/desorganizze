f8f7017d723e0f6ba90604dda8f68a173ce08537
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-04-26 21:15:33 -0400

    creating logging handler

---
+++ Desorganizze/Infra/CQRS/Commands/LoggingHandler.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+using System.Threading.Tasks;
+
+namespace Desorganizze.Infra.CQRS.Commands
+{
+    public class LoggingHandler<T> : ICommandHandler<T> where T : ICommand
+    {
+        private readonly ICommandHandler<T> next;
+
+        public LoggingHandler(ICommandHandler<T> next)
+        {
+            this.next = next;
+        }
+
+        public Task<Result> ExecuteAsync(T command)
+        {
+            Log.Information("Starting {@handler} with command: {@command}", next.GetType(), command);
+            return next.ExecuteAsync(command);
+        }
+    }
+}
