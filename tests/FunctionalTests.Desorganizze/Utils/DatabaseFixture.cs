1be5d808a16f635db5945882283c2d7e86b85ee6
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-05-03 21:54:35 -0400

    adding docker compose

--- tests/FunctionalTests.Desorganizze/Utils/DatabaseFixture.cs
+++ tests/FunctionalTests.Desorganizze/Utils/DatabaseFixture.cs
@@ -5,16 +5,16 @@ namespace FunctionalTests.Desorganizze.Utils
 {
     public class DatabaseFixture : IDisposable
     {
-        protected ThrowawayDatabase Database { get; private set; }
+        //protected ThrowawayDatabase Database { get; private set; }
 
         public DatabaseFixture()
         {
-            Database = ThrowawayDatabase.Create(connectionString: "Server=localhost;Port=5433;User Id=postgres;Password=postgres;", databaseNamePrefix: "desorganizze_test_");
+            //Database = ThrowawayDatabase.Create(connectionString: "Server=localhost;Port=5433;User Id=postgres;Password=postgres;", databaseNamePrefix: "desorganizze_test_");
         }
 
         public void Dispose()
         {
-            Database.Dispose();
+            //Database.Dispose();
         }
     }
 }
