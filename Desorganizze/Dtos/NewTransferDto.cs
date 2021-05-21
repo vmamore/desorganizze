173c0e8182b66d97bd65eff5340022fde8d21e29
Vinicius Mamoré <vinicius.mamore@gmail.com>
2021-05-21 00:49:40 -0400

    Creating new action to transfer between wallets

---
+++ Desorganizze/Dtos/NewTransferDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Desorganizze.Dtos
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class NewTransferDto
+    {
+        [Required]
+        public Guid WalletId { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal Amount { get; set; }
+    }
+}
